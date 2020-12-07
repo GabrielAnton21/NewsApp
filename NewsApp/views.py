2cc65be3265709c1a2d29a2a3206abb74dcfdb5b
GabrielAnton21 <44251263+GabrielAnton21@users.noreply.github.com>
2020-12-07 17:20:14 -0500

    added login

--- NewsApp/views.py
+++ NewsApp/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'NewsApp/index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+
+    context = {'form': form}
+    return render(request, 'NewsApp/register.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+
+    context = {'form': form}
+    return render(request, 'NewsApp/login.html', context)
+
+
+def logout(request):
+    logout(request)
+    return redirect('login')
