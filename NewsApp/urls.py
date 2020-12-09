4daa5a58a8d285582f7cf9253c01ce2a4294324a
Adam Kachidurian <66389881+TheUnhappyTriangle@users.noreply.github.com>
2020-12-09 12:51:48 -0500

    slight css changes

--- NewsApp/urls.py
+++ NewsApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name="index"),
+    path('', views.index, name="index"),
     path('register/', views.register, name="register"),
     path('login/', views.login_view, name="login"),
     path('logout/',  views.logout_view, name="logout")
